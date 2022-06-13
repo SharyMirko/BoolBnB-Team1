292292f11ec55e7a222b6066df867cdeb3aa21f0
8devil3 <8dev.il3@gmail.com>
2022-06-13 11:57:58 +0200

    aggiornato footer

--- resources/views/components/foot.blade.php
+++ resources/views/components/foot.blade.php
@@ -1,43 +1,57 @@
 <footer>
-    <div class="container">
-        <footer class="row row-cols-5 py-5 my-5 border-top">
-          <div class="col-3">
-            <h5>LOGO</h5>
-            <p>BoolBnB è una piattaforma che ti consente di affittare il tuo immobile in modo semplice e veloce.</p>
-            <a href="#" class="p-0 text-muted">Continua a leggere...</a></li>
-          </div>
-      
-          <div class="col">
+   <div class="container">
+      <div class="row py-5">
+
+         <div class="col-12 col-md-3 me-md-5 mb-4">
+            <img src="{{ asset('img/logo_light.svg') }}" alt="BoolBnB" class="logo">
+            <p class="my-3">BoolBnB è una piattaforma che ti consente di affittare il tuo immobile in modo semplice e veloce.</p>
+         </div>
+
+         <div class="col-12 col-sm-3 col-md-2 mb-4">
             <h5>Categorie</h5>
-            <ul class="nav flex-column">
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Appartamenti</a></li>
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Stanze</a></li>
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Ville singole</a></li>
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Villette</a></li>
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Casali</a></li>
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Tutte le categorie</a></li>
-            </ul>
-          </div>
-      
-          <div class="col">
+            <nav>
+               <ul class="nav flex-column">
+                  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Appartamenti</a></li>
+                  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Stanze</a></li>
+                  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Ville singole</a></li>
+                  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Villette</a></li>
+                  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Casali</a></li>
+               </ul>
+            </nav>
+
+         </div>
+
+         <div class="col-12 col-sm-3 col-md-2 mb-4">
             <h5>Login</h5>
-            <ul class="nav flex-column">
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Login</a></li>
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Registrati</a></li>
-            </ul>
-          </div>
-      
-          <div class="col">
+            <nav>
+               <ul class="nav flex-column">
+                  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Login</a></li>
+                  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Registrati</a></li>
+               </ul>
+            </nav>
+         </div>
+
+         <div class="col-12 col-sm-3 col-md-2">
             <h5>Contatti</h5>
-            <ul class="nav flex-column">
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">info@boolbnb.it</a></li>
-            </ul>
-          </div>
-        </footer>
+            <nav>
+               <ul class="nav flex-column">
+                  <li class="nav-item mb-2"><a href="#" class="nav-link p-0">info@boolbnb.it</a></li>
+               </ul>
+            </nav>
+         </div>
       </div>
-      <div class="p-3 mb-2 bg-dark text-white">
-        <div class="container">
-            <p>© BoolBnB | Design and Developed by Team 1 Boolean</p>
-        </div>
+      </div>
+
+      <div class="p-3" id="colophon">
+      <div class="container">
+            <div class="row g-0">
+               <div class="col-auto me-auto">
+                  <p class="m-0">&copy; BoolBnB | Design and Developed by Team 1 Boolean</p>
+               </div>
+               <div class="col-6 col-md-3 mt-2 m-md-0">
+                  <img src="{{ asset('img/payments_colophon.svg') }}" alt="Braintree Payments">
+               </div>
+            </div>
+      </div>
       </div>
 </footer>

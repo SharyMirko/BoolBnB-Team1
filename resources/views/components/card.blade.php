e836b2656c6b428de4d623e66bc2b53dc8e3cb95
8devil3 <8dev.il3@gmail.com>
2022-06-13 16:49:11 +0200

    creata card blade

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,17 @@
+<article class="card">
+   <div class="img_card position-relative">
+      <img src="https://picsum.photos/400/300" class="card-img-top" alt="lorem_picsum">
+
+      <div class="crown"></div>
+
+      <div class="user-avatar position-absolute"><span class="m-0 p-0">CB</span></div>
+   </div>
+   <div class="card-body">
+      <h5 class="card-title">Appartamento moderno</h5>
+      <p class="geo icon">Milano</p>
+      <div class="d-flex justify-content-between align-items-end">
+         <p class="price m-0">€ 420<span class="price-suffix">/notte</span></p>
+         <p class="category m-0">Appartamento</p>
+      </div>
+   </div>
+</article>
--- resources/views/guests/landing.blade.php
+++ resources/views/guests/landing.blade.php
@@ -19,10 +19,12 @@
    {{-- / --}}
 
    <main class="container py-4 mt-5 g-0">
+
+      {{-- appartamenti in evidenza --}}
       <section class="container my-4 py-4" id="sec_ap_prem">
          <div class="row py-3">
                <div class="col">
-                  <h3>Titolo H3</h3>
+                  <h3>Appartamenti in evidenza</h3>
                </div>
          </div>
          <div class="row py-3">
@@ -32,84 +34,48 @@
                </div>
          </div>
          <div class="row row-cols-3 py-3">
-               <div class="col">
-                  <div class="card">
-                     <div class="img_card position-relative">
-                           <img src="https://picsum.photos/400/300" class="card-img-top" alt="lorem_picsum">
-                           <div class="crown position-absolute top-0 end-0">&#128081;</div>
-                           <div class="price_night position-absolute bottom-0 start-0 text-light">--db value-- / notte</div>
-                           <div class="user_img position-absolute bottom-0 end-0 text-light">CB</div>
-                     </div>
-                     <div class="card-body">
-                           <h5 class="card-title">Titolo card</h5>
-                           <div class="card-text">
-                              <p>Luogo</p>
-                              <p>Tipo</p>
-                           </div>
-                     </div>
-                  </div>
-               </div>
-               <div class="col">
-                  <div class="card">
-                     <div class="img_card position-relative">
-                           <img src="https://picsum.photos/400/300" class="card-img-top" alt="lorem_picsum">
-                           <div class="crown position-absolute top-0 end-0">&#128081;</div>
-                           <div class="price_night position-absolute bottom-0 start-0 text-light">--db value-- / notte</div>
-                           <div class="user_img position-absolute bottom-0 end-0 text-light">CB</div>
-                     </div>
-                     <div class="card-body">
-                           <h5 class="card-title">Titolo card</h5>
-                           <div class="card-text">
-                              <p>Luogo</p>
-                              <p>Tipo</p>
-                           </div>
-                     </div>
-                  </div>
-               </div>
-               <div class="col">
-                  <div class="card">
-                     <div class="img_card position-relative">
-                           <img src="https://picsum.photos/400/300" class="card-img-top" alt="lorem_picsum">
-                           <div class="crown position-absolute top-0 end-0">&#128081;</div>
-                           <div class="price_night position-absolute bottom-0 start-0 text-light">--db value-- / notte</div>
-                           <div class="user_img position-absolute bottom-0 end-0 text-light">CB</div>
-                     </div>
-                     <div class="card-body">
-                           <h5 class="card-title">Titolo card</h5>
-                           <div class="card-text">
-                              <p>Luogo</p>
-                              <p>Tipo</p>
-                           </div>
-                     </div>
-                  </div>
-               </div>
+            <div class="col">
+               <x-card />
+            </div>
+            <div class="col">
+               <x-card />
+            </div>
+            <div class="col">
+               <x-card />
+            </div>
          </div>
       </section>
+      {{-- / --}}
 
+
+      {{-- location principali --}}
       <section classes="container my-4 py-4" id="sec_loc_prin">
          <div class="row text-center my-3">
                <h3>Location principali</h3>
          </div>
          <div class="row row-cols-2 g-4 text-center">
                <div class="col position-relative">
-                  <img src="https://picsum.photos/500/400" class="" alt="lorem_picsum">
+                  <img src="https://picsum.photos/500/400?random=1" class="" alt="lorem_picsum">
                   <p class="position-absolute bottom-0 start-50 translate-middle-x text-light">Città</p>
                </div>
                <div class="col position-relative">
-                  <img src="https://picsum.photos/500/400" class="" alt="lorem_picsum">
+                  <img src="https://picsum.photos/500/400?random=2" class="" alt="lorem_picsum">
                   <p class="position-absolute bottom-0 start-50 translate-middle-x text-light">Città</p>
                </div>
                <div class="col position-relative">
-                  <img src="https://picsum.photos/500/400" class="" alt="lorem_picsum">
+                  <img src="https://picsum.photos/500/400?random=3" class="" alt="lorem_picsum">
                   <p class="position-absolute bottom-0 start-50 translate-middle-x text-light">Città</p>
                </div>
                <div class="col position-relative">
-                  <img src="https://picsum.photos/500/400" class="" alt="lorem_picsum">
+                  <img src="https://picsum.photos/500/400?random=4" class="" alt="lorem_picsum">
                   <p class="position-absolute bottom-0 start-50 translate-middle-x text-light">Città</p>
                </div>
          </div>
       </section>
+      {{-- / --}}
 
+
+      {{-- categorie --}}
       <section class="container my-4 py-4" id="sec_categorie">
          <div class="row text-center">
                <h3>Categorie</h3>
@@ -132,11 +98,7 @@
                   <p class="position-absolute bottom-0 start-50 translate-middle-x text-light">Città</p>
                </div>
          </div>
-         <div class="row text-center my-4">
-               <div class="col">
-                  <button class="btn btn-danger">Tutte le categorie</button>
-               </div>
-         </div>
       </section>
+      {{-- / --}}
    </main>
 @endsection

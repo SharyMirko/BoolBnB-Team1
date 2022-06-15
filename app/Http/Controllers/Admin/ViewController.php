c3079bf724b4b286f5273089f5b1ef5db4fe86e2
Neeiser <stortini.study@gmail.com>
2022-06-15 16:07:48 +0200

    View (statistics) index + store comments

--- app/Http/Controllers/Admin/ViewController.php
+++ app/Http/Controllers/Admin/ViewController.php
@@ -9,77 +9,52 @@ use App\Http\Controllers\Controller;
 
 class ViewController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function index()
     {
-        //
+        return view('admin.statistics.index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    
     public function store(Request $request)
     {
-        //
+        /* 
+
+        Ipotesi memorizzazione visualizzazione:
+        https://laravel.com/docs/7.x/http-client#introduction
+        
+        1. Controllo IP dell'utente;
+        2. Controllo status risposta: $response->status() : int;
+        3. Se le 2 condizioni precedenti sono valide avviare procedura store.
+        
+        */
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\View  $view
-     * @return \Illuminate\Http\Response
-     */
+    
     public function show(View $view)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\View  $view
-     * @return \Illuminate\Http\Response
-     */
+    
     public function edit(View $view)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\View  $view
-     * @return \Illuminate\Http\Response
-     */
+    
     public function update(Request $request, View $view)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\View  $view
-     * @return \Illuminate\Http\Response
-     */
+    
     public function destroy(View $view)
     {
         //

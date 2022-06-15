8f4e4fdeb91eae7888ea26457221f4c80de65df0
Neeiser <stortini.study@gmail.com>
2022-06-15 15:51:00 +0200

    Crud messages

--- app/Http/Controllers/Admin/MessageController.php
+++ app/Http/Controllers/Admin/MessageController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers\Admin;
 use App\Model\Message;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class MessageController extends Controller
 {
     
     public function index()
     {
-        //
+        $message = Message::where('user_id', Auth::user()->id)->all(20);
+
+        return view('admin.messages.index', compact('message'));
     }
 
     
@@ -23,7 +26,13 @@ class MessageController extends Controller
     
     public function store(Request $request)
     {
-        //
+        $messageForm = $request->all();
+
+        $message = new Message();
+        $message->fill($messageForm);
+        $message->save();
+
+        return redirect()->route('apartments.show');
     }
 
     

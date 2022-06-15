f019d89d29ce9116bd5dc15977a69bf9daaa0da6
8devil3 <8dev.il3@gmail.com>
2022-06-15 17:45:27 +0200

    corretta card

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,17 +1,19 @@
 <article class="card rounded-3 border-0">
-   <div class="img_card position-relative">
-      <img src="https://picsum.photos/400/300" class="card-img-top" alt="lorem_picsum">
+   <a href="#">
+      <div class="img_card position-relative">
+         <img src="https://picsum.photos/400/300" class="card-img-top" alt="lorem_picsum">
 
-      <div class="crown"></div>
+         <div class="crown"></div>
 
-      <div class="user-avatar position-absolute"><span class="m-0 p-0">CB</span></div>
-   </div>
-   <div class="card-body">
-      <h5 class="card-title">Appartamento moderno</h5>
-      <p class="geo icon">Milano</p>
-      <div class="d-flex justify-content-between align-items-end flex-wrap">
-         <p class="price m-0">€ 420<span class="price-suffix">/notte</span></p>
-         <p class="category m-0">Appartamento</p>
+         <div class="user-avatar position-absolute"><span class="m-0 p-0">CB</span></div>
       </div>
-   </div>
+      <div class="card-body">
+         <h5 class="card-title">Appartamento moderno</h5>
+         <p class="geo icon">Milano</p>
+         <div class="d-flex justify-content-between align-items-end flex-wrap">
+            <p class="price m-0">€ 420<span class="price-suffix">/notte</span></p>
+            <p class="category m-0">Appartamento</p>
+         </div>
+      </div>
+   </a>
 </article>

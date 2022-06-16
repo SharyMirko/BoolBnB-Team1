68c3b252c82a63bfdafb4a87e71a76c5c6d97508
Mirko Moschella <shary.mirko@gmail.com>
2022-06-16 12:28:50 +0200

    controllo

--- app/Http/Controllers/Admin/ViewController.php
+++ app/Http/Controllers/Admin/ViewController.php
@@ -27,67 +27,26 @@ class ViewController extends Controller
         //
     }
 
-<<<<<<< HEAD:app/Http/Controllers/Admin/ViewController.php
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\View  $view
-     * @return \Illuminate\Http\Response
-     */
-    public function show(View $view)
-=======
     
     public function show(Statistic $statistic)
->>>>>>> CRUD:app/Http/Controllers/Admin/StatisticController.php
     {
         //
     }
 
-<<<<<<< HEAD:app/Http/Controllers/Admin/ViewController.php
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\View  $view
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(View $view)
-=======
     
     public function edit(Statistic $statistic)
->>>>>>> CRUD:app/Http/Controllers/Admin/StatisticController.php
     {
         //
     }
 
-<<<<<<< HEAD:app/Http/Controllers/Admin/ViewController.php
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\View  $view
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, View $view)
-=======
     
     public function update(Request $request, Statistic $statistic)
->>>>>>> CRUD:app/Http/Controllers/Admin/StatisticController.php
     {
         //
     }
 
-<<<<<<< HEAD:app/Http/Controllers/Admin/ViewController.php
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\View  $view
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(View $view)
-=======
     
     public function destroy(Statistic $statistic)
->>>>>>> CRUD:app/Http/Controllers/Admin/StatisticController.php
     {
         //
     }

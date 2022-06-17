3dd8cb756940576c9a08f7a9c54579d7c255c8a8
xVal4k <cristianbricicaru.cb@gmail.com>
2022-06-17 16:39:51 +0200

    api semi-done  -  need fix

---
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Model\Apartment;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ApartmentController extends Controller
+{
+   /**
+    * Display a listing of the resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+   public function index(Request $request)
+   {
+      // $attributes = $request->all();
+
+      $apiApartments = DB::table('apartments')
+         ->join('users', 'apartments.user_id', 'users.id')
+         ->join('apartment_service', 'apartments.id', 'apartment_service.apartment_id')
+         ->join('services', 'services.id', 'apartment_service.service_id')
+         ->select('apartments.*', 'users.first_name', 'users.last_name', 'services.name as services')
+         ->orderBy('id', 'asc')
+         ->paginate(20);
+
+      return response()->json([
+         'success'   => true,
+         'response'  => [
+            'data'      => $apiApartments
+         ]
+      ]);
+   }
+
+   /**
+    * Show the form for creating a new resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+   public function create()
+   {
+      //
+   }
+
+   /**
+    * Store a newly created resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @return \Illuminate\Http\Response
+    */
+   public function store(Request $request)
+   {
+      //
+   }
+
+   /**
+    * Display the specified resource.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+   public function show($id)
+   {
+      $apartment = Apartment::where('id', $id)->first();
+      if ($apartment) {
+         return response()->json([
+            'success'   => true,
+            'response'  => [
+               'data'      => $apartment,
+            ]
+         ]);
+      } else {
+         return response()->json([
+            'success'   => false,
+         ]);
+      }
+   }
+
+   /**
+    * Show the form for editing the specified resource.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+   public function edit($id)
+   {
+      //
+   }
+
+   /**
+    * Update the specified resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+   public function update(Request $request, $id)
+   {
+      //
+   }
+
+   /**
+    * Remove the specified resource from storage.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+   public function destroy($id)
+   {
+      //
+   }
+}

c568c226442c51cde79cdb81e2cfedc99e979c19
8devil3 <8dev.il3@gmail.com>
2022-06-27 16:49:04 +0200

    coretti link

--- resources/views/guests/landing.blade.php
+++ resources/views/guests/landing.blade.php
@@ -81,25 +81,25 @@
       </div>
       <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4 text-center">
             <div class="col">
-               <a href="{{ route('admin.apartments.index') }}" class="text-muted">
+               <a href="{{ route('apartments.index') }}" class="text-muted">
                   <img src="{{ asset('img/cat_appartamenti.png') }}" class="img-fluid w-100" alt="Appartamenti">
                   <p class="landing-category-name">Appartamenti</p>
                </a>
             </div>
             <div class="col">
-               <a href="{{ route('admin.apartments.index') }}" class="text-muted">
+               <a href="{{ route('apartments.index') }}" class="text-muted">
                   <img src="{{ asset('img/cat_stanze.png') }}" class="img-fluid w-100" alt="Stanze">
                   <p class="landing-category-name">Stanze</p>
                </a>
             </div>
             <div class="col">
-               <a href="{{ route('admin.apartments.index') }}" class="text-muted">
+               <a href="{{ route('apartments.index') }}" class="text-muted">
                   <img src="{{ asset('img/cat_casali.png') }}" class="img-fluid w-100" alt="Casali">
                   <p class="landing-category-name">Casali</p>
                </a>
             </div>
             <div class="col">
-               <a href="{{ route('admin.apartments.index') }}" class="text-muted">
+               <a href="{{ route('apartments.index') }}" class="text-muted">
                   <img src="{{ asset('img/cat_ville.png') }}" class="img-fluid w-100" alt="Ville">
                   <p class="landing-category-name">Ville</p>
                </a>
